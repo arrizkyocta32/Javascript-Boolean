2c9efa02ccdc370a9fe1c9f3eb86fcfae3e52065
arrizkyocta32 <144558025+arrizkyocta32@users.noreply.github.com>
2024-02-04 20:44:33 +0700

    Add files via upload

---
+++ example1.js
@@ -0,0 +1,8 @@
+let isCoffeeSold = false;
+let isMenuAvailable= true;
+
+if(isCoffeeSold|| isMenuAvailable) {
+    console.log('coffee is available');
+} else {
+    console.log('coffee is not available');
+}
