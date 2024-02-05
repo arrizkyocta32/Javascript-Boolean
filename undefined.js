890efa576f14b95dd7a39d4a141353b780cd7f6b
arrizkyocta32 <144558025+arrizkyocta32@users.noreply.github.com>
2024-02-05 11:26:56 +0700

    Update undefined.js

--- undefined.js
+++ undefined.js
@@ -9,3 +9,32 @@ let x;
 
 console.log(test(x));
 // Expected output: "Undefined value!"
+
+// DON'T DO THIS
+
+(() => {
+  const undefined = "foo";
+  console.log(undefined, typeof undefined); // foo string
+})();
+
+((undefined) => {
+  console.log(undefined, typeof undefined); // foo string
+})("foo");
+
+//STRICT EQUALITY AND UNDEFINED
+let x;
+if (x === undefined) {
+  // these statements execute
+} else {
+  // these statements do not execute
+} 
+
+// x has not been declared before
+// evaluates to true without errors
+if (typeof x === "undefined") {
+  // these statements execute
+}
+
+// Throws a ReferenceError
+if (x === undefined) {
+}
