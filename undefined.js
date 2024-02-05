c1c9897161c1300943fdf981a6faccb1e6b497ee
arrizkyocta32 <144558025+arrizkyocta32@users.noreply.github.com>
2024-02-05 12:22:36 +0700

    Update undefined.js

--- undefined.js
+++ undefined.js
@@ -38,3 +38,36 @@ if (typeof x === "undefined") {
 // Throws a ReferenceError
 if (x === undefined) {
 }
+
+type of OPERATOR AND UNDEFINED
+let x;
+if (typeof x === "undefined") {
+  // these statements execute
+}  
+// x has not been declared before
+// evaluates to true without errors
+if (typeof x === "undefined") {
+  // these statements execute
+}
+
+// Throws a ReferenceError
+if (x === undefined) {
+} 
+
+operator instance
+if ("x" in window) {
+  // These statements execute only if x is defined globally
+}
+
+void operator and undefined
+The void operator is a third alternative
+
+let x;
+if (x === void 0) {
+  // these statements execute
+}
+
+// y has not been declared before
+if (y === void 0) {
+  // throws Uncaught ReferenceError: y is not defined
+}
